3e5471b92c1c292213654225fc70aa25622c6b46
kaizodevx <mandaalv72@gmail.com>
2026-03-02 11:11:09 +0800

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+  schema: "./src/server/db/schema/",
+  dialect: "turso",
+  dbCredentials: {
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
+  },
+  tablesFilter: ["aurafy_*"],
+} satisfies Config;
